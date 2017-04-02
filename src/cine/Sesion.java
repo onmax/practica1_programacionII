33f2ee41068e9b90aaa252db2232936123528204
Máximo J. García Martínez <maximo.garcia.martinez@alumnos.upm.es>
2017-04-02 16:55:54 +0200

    Actualizado

--- src/cine/Sesion.java
+++ src/cine/Sesion.java
@@ -4,18 +4,22 @@ public class Sesion {
 	private String hora;
 	// hora de la sesión en formato HH:MM
 	private int asientosDisponibles;
+	//es un entero que indica el número de asientos disponibles en la propia sesión
+	private int sigIdCompra;
+	//es un entero que se incrementa cada vez que se realiza una venta de entradas para la propia sesión. Se utiliza para generar identificadores de venta diferentes para cada compra
+	private int [][] estadoAsientos;
 	//es una matriz de enteros con las mismas dimensiones que la sala asociada a la propia sesión, en la que se representan las butacas compradas y las disponibles para la propia sesión. 
 	//Si una butaca en la posición (i, j) está disponible, se guarda un valor 0, y en caso contrario, se guarda el identificador de venta asociado a la butaca. Como en una venta se puede comprar más de una butaca, 
 	//podría suceder que en la matriz haya varios identificadores de ventas iguales en posiciones contiguas. 
-	private int sigIdCompra;
-	//es un entero que indica el número de asientos disponibles en la propia sesión
-	private int [][] estadoAsientos;
-	//es un entero que se incrementa cada vez que se realiza una venta de entradas para la propia sesión. Se utiliza para generar identificadores de venta diferentes para cada compra. 
+
 	
 	public Sesion (String hora, int fila, int columnas){
 	//constructor de la clase Sesion que recibe como argumentos la hora de proyección, y el número de filas y columnas de la sala asociada a la propia sesión. Con estos argumentos, 
 	//inicializa los atributos del objeto. El atributo sigIdComprase inicializa a 1. 
 		this.hora = hora;
+		this.estadoAsientos = new int [fila][columnas];
+		this.asientosDisponibles = fila * columnas;
+		this.sigIdCompra = 1;
 	}
 	
 	public String getHora(){
@@ -36,7 +40,8 @@ public class Sesion {
 	//método que compra una entrada con la fila y columna dadas para la propia sesión. Para registrar la venta, se guarda el valor actual del atributo sigIdCompra en la posición 
 	//(fila-1, columna-1) del atributo estadoAsientos. A continuación, se incrementa en uno el atributo sigIdCompra.
 		this.estadoAsientos[fila - 1][columna - 1]= sigIdCompra;
-		sigIdCompra += 1;
+		this.sigIdCompra += 1;
+		this.asientosDisponibles -= 1;
 	}
 	
 	public int getIdEntrada(int fila, int columna){
@@ -64,6 +69,10 @@ public class Sesion {
 	
 	public int getButacasDisponiblesSesion(){
 	//método que devuelve el número de butacas disponibles en la propia sesión.
+		//OPCION 1: Para esta opción hace falta añadir <-- this.asientosDisponibles -= 1; --> en comprarEntrada, pero no viene en el pdf.
+		//return this.asientosDisponibles;
+		
+		//OPCION 2
 		int res = 0;
 		char estadoSesion [][] = getEstadoSesion();
 		for(int i= 0; i<this.estadoAsientos.length; i++){
@@ -79,15 +88,31 @@ public class Sesion {
 	public String recogerEntradas(int id){
 	//método que devuelve las N entradas asociadas a un identificador de venta dado para la propia sesión. Las N entradas se devolverán dentro de un String con el siguiente formato: 
 	//“hora_de_la_sesión+fila1,columna1+fila2,columna2+ … +filaN,columnaN+” Si el identificador de venta dado no existe en la propia sesión, se devuelve null.
-		String res = this.getHora();
+		String res = this.getHora() + "+";
+		boolean bool = false;
+		for(int i= 0; i<this.estadoAsientos.length; i++){
+			for (int j = 0; j< this.estadoAsientos[i].length;j++){
+				if(this.estadoAsientos[i][j] == id){
+					bool = true;
+					res += i + "," + j + "+";
+				}
+			}
+		}
+		if(!bool){
+			return null;
+		}else{
+			return res;
+		}
 	}
 	
-	public recomendarButacasContiguas(){
+	public ButacasContiguas recomendarButacasContiguas(int noButacas){
 	// método que dados un número N de butacas, devuelve un objeto de tipo ButacasContiguas que contiene la fila y la columna de la butaca recomendaba con menor número de columna, y el número de butacas solicitadas. 
 	//El algoritmo para obtener las butacas recomendadas es el siguiente: 1. Se buscan las primeras N butacas contiguas libres empezando por la fila (N_FILAS+1)/2+1 y acabando en la fila N_FILAS. Cada fila se recorre 
 	//empezando por la columna N_COLUMNAS y acabando en la columna 1. 2. Si en el paso anterior no se encuentran N butacas contiguas libres, se buscan las primeras N butacas contiguas libres empezando por la fila 
 	//(N_FILAS+1)/2 y acabando en la fila 1. Cada fila se recorre empezando por la columna N_COLUMNAS y acabando en la columna 1. Si no existen N butacas libres contiguas en la sala para la propia sesión, se devuelve null.
-	//Por ejemplo, si tomamos la sala de la figura 2 y buscamos 3 butacas contiguas libres, este algoritmo devolverá las butacas (4, 3), (4, 4) y (4, 5). 
+	//Por ejemplo, si tomamos la sala de la figura 2 y buscamos 3 butacas contiguas libres, este algoritmo devolverá las butacas (4, 3), (4, 4) y (4, 5).
+		ButacasContiguas recomendadas = new ButacasContiguas(1,2,3);
+		return recomendadas;
 	}
 	
 	public void comprarEntradasRecomendadas(ButacasContiguas butacas){
