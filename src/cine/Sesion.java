970b4079b0c3013825043a09cf82e9253bbbb772
Javitemek <javierbarraganh@gmail.com>
2017-04-04 16:29:07 +0200

    Actualización 1 ButacasContiguas
    Tu método estaba incompleto, lo he vuelto a hacer, mira que este bien

--- src/cine/Sesion.java
+++ src/cine/Sesion.java
@@ -1,22 +1,22 @@
 package cine;
 
-//@Programacion2(nombreAutor1 = "M·ximo", apellidoAutor1 = "GarcÌa MartÌnez", emailUPMAutor1 = "maximo.garcia.martinez@alumnos.upm.es", 
+//@Programacion2(nombreAutor1 = "M√°ximo", apellidoAutor1 = "Garc√≠a Mart√≠nez", emailUPMAutor1 = "maximo.garcia.martinez@alumnos.upm.es", 
 //				nombreAutor2 = "Javier", apellidoAutor2 = "Barragan Haro", emailUPMAutor2 = "javier.barrragan.haro@alumnos.upm.es");
 public class Sesion {
 	private String hora;
-	// hora de la sesiÛn en formato HH:MM
+	// hora de la sesi√≥n en formato HH:MM
 	private int asientosDisponibles;
-	//es un entero que indica el n˙mero de asientos disponibles en la propia sesiÛn
+	//es un entero que indica el n√∫mero de asientos disponibles en la propia sesi√≥n
 	private int sigIdCompra;
-	//es un entero que se incrementa cada vez que se realiza una venta de entradas para la propia sesiÛn. Se utiliza para generar identificadores de venta diferentes para cada compra
+	//es un entero que se incrementa cada vez que se realiza una venta de entradas para la propia sesi√≥n. Se utiliza para generar identificadores de venta diferentes para cada compra
 	private int [][] estadoAsientos;
-	//es una matriz de enteros con las mismas dimensiones que la sala asociada a la propia sesiÛn, en la que se representan las butacas compradas y las disponibles para la propia sesiÛn. 
-	//Si una butaca en la posiciÛn (i, j) est· disponible, se guarda un valor 0, y en caso contrario, se guarda el identificador de venta asociado a la butaca. Como en una venta se puede comprar m·s de una butaca, 
-	//podrÌa suceder que en la matriz haya varios identificadores de ventas iguales en posiciones contiguas. 
+	//es una matriz de enteros con las mismas dimensiones que la sala asociada a la propia sesi√≥n, en la que se representan las butacas compradas y las disponibles para la propia sesi√≥n. 
+	//Si una butaca en la posici√≥n (i, j) est√° disponible, se guarda un valor 0, y en caso contrario, se guarda el identificador de venta asociado a la butaca. Como en una venta se puede comprar m√°s de una butaca, 
+	//podr√≠a suceder que en la matriz haya varios identificadores de ventas iguales en posiciones contiguas. 
 
 	
 	public Sesion (String hora, int fila, int columnas){
-	//constructor de la clase Sesion que recibe como argumentos la hora de proyecciÛn, y el n˙mero de filas y columnas de la sala asociada a la propia sesiÛn. Con estos argumentos, 
+	//constructor de la clase Sesion que recibe como argumentos la hora de proyecci√≥n, y el n√∫mero de filas y columnas de la sala asociada a la propia sesi√≥n. Con estos argumentos, 
 	//inicializa los atributos del objeto. El atributo sigIdComprase inicializa a 1. 
 		this.hora = hora;
 		this.estadoAsientos = new int [fila][columnas];
@@ -25,18 +25,18 @@ public class Sesion {
 	}
 	
 	public String getHora(){
-		//mÈtodo que devuelve la hora asociada a la propia sesiÛn
+		//m√©todo que devuelve la hora asociada a la propia sesi√≥n
 		return this.hora;
 	}
 	
 	public boolean equals(Sesion obj){
-		//mÈtodo que compara el objeto de tipo Sesion dado con la propia sesiÛn, y devuelve cierto si son iguales y falso en caso contrario. Se considera que dos sesiones son iguales si son iguales sus atributos hora.  
+		//m√©todo que compara el objeto de tipo Sesion dado con la propia sesi√≥n, y devuelve cierto si son iguales y falso en caso contrario. Se considera que dos sesiones son iguales si son iguales sus atributos hora.  
 		return obj.getHora() == this.hora;
 	}
 	
 	public void comprarEntrada(int fila, int columna){
-	//mÈtodo que compra una entrada con la fila y columna dadas para la propia sesiÛn. Para registrar la venta, se guarda el valor actual del atributo sigIdCompra en la posiciÛn 
-	//(fila-1, columna-1) del atributo estadoAsientos. A continuaciÛn, se incrementa en uno el atributo sigIdCompra.
+	//m√©todo que compra una entrada con la fila y columna dadas para la propia sesi√≥n. Para registrar la venta, se guarda el valor actual del atributo sigIdCompra en la posici√≥n 
+	//(fila-1, columna-1) del atributo estadoAsientos. A continuaci√≥n, se incrementa en uno el atributo sigIdCompra.
 		if(estadoAsientos[fila - 1][columna - 1] == 0){
 			this.estadoAsientos[fila - 1][columna - 1]= sigIdCompra;
 			this.sigIdCompra += 1;
@@ -45,15 +45,15 @@ public class Sesion {
 	}
 	
 	public int getIdEntrada(int fila, int columna){
-	// mÈtodo que devuelve el identificador de venta para una entrada en la propia sesiÛn especificada mediante su fila y columna. El identificador que se devuelve se toma de la posiciÛn 
+	// m√©todo que devuelve el identificador de venta para una entrada en la propia sesi√≥n especificada mediante su fila y columna. El identificador que se devuelve se toma de la posici√≥n 
 	//(fila-1, columna-1) del atributo estadoAsientos
 		return this.estadoAsientos[fila - 1][columna - 1];
 	}
 	
 	public char [][] getEstadoSesion(){
-	//mÈtodo que devuelve una matriz de caracteres en la que se representa el estado de ocupaciÛn de la propia sesiÛn. La matriz resultado tiene el mismo n˙mero de filas y columnas que la sala asociada
-	//a la propia sesiÛn, y en cada posiciÛn (i, j) de la matriz debe aparecer un car·cter ë#í si la butaca ha sido comprada, y un car·cter ëOí (O may˙scula) si la butaca est· disponible. En la figura 2 se puede 
-	//observar un ejemplo de una matriz de caracteres que refleja el estado de ocupaciÛn de una sala en una sesiÛn
+	//m√©todo que devuelve una matriz de caracteres en la que se representa el estado de ocupaci√≥n de la propia sesi√≥n. La matriz resultado tiene el mismo n√∫mero de filas y columnas que la sala asociada
+	//a la propia sesi√≥n, y en cada posici√≥n (i, j) de la matriz debe aparecer un car√°cter ‚Äò#‚Äô si la butaca ha sido comprada, y un car√°cter ‚ÄòO‚Äô (O may√∫scula) si la butaca est√° disponible. En la figura 2 se puede 
+	//observar un ejemplo de una matriz de caracteres que refleja el estado de ocupaci√≥n de una sala en una sesi√≥n
 		char res [][] = new char[this.estadoAsientos.length][this.estadoAsientos[0].length];
 		for(int i= 0; i<this.estadoAsientos.length; i++){
 			for (int j = 0; j< this.estadoAsientos[i].length;j++){
@@ -68,8 +68,8 @@ public class Sesion {
 	}
 	
 	public int getButacasDisponiblesSesion(){
-	//mÈtodo que devuelve el n˙mero de butacas disponibles en la propia sesiÛn.
-		//OPCION 1: Para esta opciÛn hace falta aÒadir <-- this.asientosDisponibles -= 1; --> en comprarEntrada, pero no viene en el pdf.
+	//m√©todo que devuelve el n√∫mero de butacas disponibles en la propia sesi√≥n.
+		//OPCION 1: Para esta opci√≥n hace falta a√±adir <-- this.asientosDisponibles -= 1; --> en comprarEntrada, pero no viene en el pdf.
 		//return this.asientosDisponibles;
 		
 		//OPCION 2
@@ -86,8 +86,8 @@ public class Sesion {
 	}
 	
 	public String recogerEntradas(int id){
-	//mÈtodo que devuelve las N entradas asociadas a un identificador de venta dado para la propia sesiÛn. Las N entradas se devolver·n dentro de un String con el siguiente formato: 
-	//ìhora_de_la_sesiÛn+fila1,columna1+fila2,columna2+ Ö +filaN,columnaN+î Si el identificador de venta dado no existe en la propia sesiÛn, se devuelve null.
+	//m√©todo que devuelve las N entradas asociadas a un identificador de venta dado para la propia sesi√≥n. Las N entradas se devolver√°n dentro de un String con el siguiente formato: 
+	//‚Äúhora_de_la_sesi√≥n+fila1,columna1+fila2,columna2+ ‚Ä¶ +filaN,columnaN+‚Äù Si el identificador de venta dado no existe en la propia sesi√≥n, se devuelve null.
 		String res = this.getHora() + "+";
 		boolean bool = false;
 		for(int i= 0; i<this.estadoAsientos.length; i++){
@@ -106,11 +106,13 @@ public class Sesion {
 	}
 	
 	public ButacasContiguas recomendarButacasContiguas(int noButacas){
-	// mÈtodo que dados un n˙mero N de butacas, devuelve un objeto de tipo ButacasContiguas que contiene la fila y la columna de la butaca recomendaba con menor n˙mero de columna, y el n˙mero de butacas solicitadas. 
+	// m√©todo que dados un n√∫mero N de butacas, devuelve un objeto de tipo ButacasContiguas que contiene la fila y la columna de la butaca recomendaba con menor n√∫mero de columna, y el n√∫mero de butacas solicitadas. 
 	//El algoritmo para obtener las butacas recomendadas es el siguiente: 1. Se buscan las primeras N butacas contiguas libres empezando por la fila (N_FILAS+1)/2+1 y acabando en la fila N_FILAS. Cada fila se recorre 
 	//empezando por la columna N_COLUMNAS y acabando en la columna 1. 2. Si en el paso anterior no se encuentran N butacas contiguas libres, se buscan las primeras N butacas contiguas libres empezando por la fila 
-	//(N_FILAS+1)/2 y acabando en la fila 1. Cada fila se recorre empezando por la columna N_COLUMNAS y acabando en la columna 1. Si no existen N butacas libres contiguas en la sala para la propia sesiÛn, se devuelve null.
-	//Por ejemplo, si tomamos la sala de la figura 2 y buscamos 3 butacas contiguas libres, este algoritmo devolver· las butacas (4, 3), (4, 4) y (4, 5).
+	//(N_FILAS+1)/2 y acabando en la fila 1. Cada fila se recorre empezando por la columna N_COLUMNAS y acabando en la columna 1. Si no existen N butacas libres contiguas en la sala para la propia sesi√≥n, se devuelve null.
+	//Por ejemplo, si tomamos la sala de la figura 2 y buscamos 3 butacas contiguas libres, este algoritmo devolver√° las butacas (4, 3), (4, 4) y (4, 5).
+		
+		//--------METODO DE MAX--------//
 		int filas = (estadoAsientos.length/2) + 1;
 		int columnas = estadoAsientos[0].length - 1;
 		int cont = 0;
@@ -139,8 +141,53 @@ public class Sesion {
 		
 	}
 	
+	//--------METODO DEL PUTO AMO----------//
+	
+	public ButacasContiguas recomendarButacasContiguas(int noButacas) {
+		
+		//------------------Variable--------------------- //
+		
+		int con = 0;
+		char[][] aux = getEstadoSesion();
+		
+		// ----------------Parte1----------------------- //
+		
+		for (int i = (this.estadoAsientos.length + 1) / 2 + 1; this.estadoAsientos.length > i; i++) {
+			for (int j = this.estadoAsientos[i].length-1; j > 1; j--) {
+				if (aux[i][j] == 'O') {
+					con++;
+					if (con == noButacas) {
+						ButacasContiguas solucion = new ButacasContiguas(i, j, noButacas);
+						return solucion;
+					} // Fin if2
+				} else {
+					con = 0;
+				}
+			} // Fin for j
+		} // Fin for i
+
+		// ---------------Parte2------------------- //
+
+		for (int i = (this.estadoAsientos.length + 1) / 2; i >= 1; i--) {
+			for (int j = this.estadoAsientos[i].length; j > 1; j--) {
+				if (aux[i][j] == 'O') {
+					con++;
+					if (con == noButacas) {
+						ButacasContiguas solucion = new ButacasContiguas(i, j, noButacas);
+						return solucion;
+					} // Fin if2
+				} else {
+					con = 0;
+				} // Fin else
+			} // Fin for j
+		} // Fin for i
+
+		// ---------------Parte3---------------- //
+		
+		return null;
+	}
 	public void comprarEntradasRecomendadas(ButacasContiguas butacas){
-	//mÈtodo que dado un objeto de tipo ButacasContiguas, registra la compra en la propia sesiÛn guardando el valor actual del atributo sigIdCompra en las posiciones especificadas por el objeto dado como argumento. 
-	//A continuaciÛn, se incrementa en uno el atributo sigIdCompra. 
+	//m√©todo que dado un objeto de tipo ButacasContiguas, registra la compra en la propia sesi√≥n guardando el valor actual del atributo sigIdCompra en las posiciones especificadas por el objeto dado como argumento. 
+	//A continuaci√≥n, se incrementa en uno el atributo sigIdCompra. 
 	}
 }
