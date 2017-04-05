859096fb57bc43ed0e2ab8c51421bead7f0b77a3
Javitemek <javierbarraganh@gmail.com>
2017-04-05 16:54:28 +0200

    Update Cine 1
    Pos eso, que he hacido mazo cosas

--- src/cine/Cine.java
+++ src/cine/Cine.java
@@ -2,50 +2,62 @@ package cine;
 
 public class Cine {
 	private String nombre;
-	private int [] salas;
-	
-	public Cine(String nombre, int [] salas){
+	private Sala[] salas;
+
+	public Cine(String nombre, Sala[] salas) {
 		this.nombre = nombre;
 		this.salas = salas;
 	}
-	
-	public void comprarEntradas(int sala, int sesion, int fila, int columna){
-		
+
+	public void comprarEntradas(int sala, int sesion, int fila, int columna) {
+		this.salas[sala].comprarEntrada(sesion, fila, columna);
 	}
-	
-	public ButacasContiguas recomendarButacasContiguas(int noButacas, int sala, int sesion){
-		
+
+	public ButacasContiguas recomendarButacasContiguas(int noButacas, int sala, int sesion) {
+
 	}
-	
-	public char [][] getIdEntrada(int sala, int sesion){
-		
+
+	public int getIdEntrada(int sala, int sesion, int fila, int columna) {
+		return this.salas[sala].getIdEntrada(sesion, fila, columna);
 	}
-	
-	public String [] getPeliculas(){
-		
+
+	public String[] getPeliculas() {
+		String[] aux = new String[this.salas.length];
+		for (int i = 0; i < this.salas.length; i++) {
+			aux[i] = this.salas[i].getPelicula();
+		}
+		return aux;
 	}
-	
-	public String [] getEstadoSesionDeSala(int sala){
-		
+
+	public String[] getHorasDeSesionesDeSala(int sala) {
+		return this.salas[sala].getHorasDeSesionesDeSala();
 	}
-	
-	public String recogerEntradas(int id, int sala, int sesion){
-		
+
+	public char[][] getEstadoSesion(int sala, int sesion) {
+		return this.salas[sala].getEstadoSesion(sesion);
 	}
-	
-	public int getButacasDisponiblesSesion(int sala, int sesion){
-		
+
+	public String[] getEstadoSesionDeSala(int sala) {
+
 	}
-	
-	public void comprarEntradasRecomendadas(int sala, int sesion, ButacasContiguas butacas){
-		
+
+	public String recogerEntradas(int id, int sala, int sesion) {
+
 	}
-	
-	public void incluirSesion(int sala, String horaSesion){
-		
+
+	public int getButacasDisponiblesSesion(int sala, int sesion) {
+		return this.salas[sala].getButacasDisponiblesSesion(sesion);
 	}
-	
-	public void borrarSesion(int sala, String horaSesion){
-		
+
+	public void comprarEntradasRecomendadas(int sala, int sesion, ButacasContiguas butacas) {
+
+	}
+
+	public void incluirSesion(int sala, String horaSesion) {
+
+	}
+
+	public void borrarSesion(int sala, String horaSesion) {
+
 	}
 }
