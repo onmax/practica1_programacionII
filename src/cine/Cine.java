aea295a64da1a0b840ea78eb166066f5be3ca934
Javitemek <javierbarraganh@gmail.com>
2017-04-10 12:03:23 +0200

    Update Cine.java

--- src/cine/Cine.java
+++ src/cine/Cine.java
@@ -10,13 +10,13 @@ public class Cine {
 	}
 
 	public void comprarEntrada(int sala, int sesion, int fila, int columna) {
-		this.salas[sala].comprarEntrada(sesion, fila, columna);
+		this.salas[sala - 1].comprarEntrada(sesion, fila, columna);
 	}
-	
+
 	public int getIdEntrada(int sala, int sesion, int fila, int columna) {
-		return this.salas[sala].getIdEntrada(sesion, fila, columna);
+		return this.salas[sala - 1].getIdEntrada(sesion, fila, columna);
 	}
-	
+
 	public String[] getPeliculas() {
 		String[] aux = new String[this.salas.length];
 		for (int i = 0; i < this.salas.length; i++) {
@@ -24,41 +24,42 @@ public class Cine {
 		}
 		return aux;
 	}
-	
+
 	public String[] getHorasDeSesionesDeSala(int sala) {
-		return this.salas[sala].getHorasDeSesionesDeSala();
+		return this.salas[sala - 1].getHorasDeSesionesDeSala();
 	}
-	
+
 	public char[][] getEstadoSesion(int sala, int sesion) {
-		return this.salas[sala].getEstadoSesion(sesion);
+		return this.salas[sala - 1].getEstadoSesion(sesion);
 	}
-	
+
 	public int getButacasDisponiblesSesion(int sala, int sesion) {
-		return this.salas[sala].getButacasDisponiblesSesion(sesion);
+		return this.salas[sala - 1].getButacasDisponiblesSesion(sesion);
 	}
-	
+
 	public String recogerEntradas(int id, int sala, int sesion) {
 		String info = this.salas[sala].recogerEntradas(id, sesion);
-		if(info == null){
+		if (info != null) {
 			return this.nombre + "@" + info;
-		}else{
+		} else {
 			return null;
 		}
 	}
 
 	public ButacasContiguas recomendarButacasContiguas(int noButacas, int sala, int sesion) {
-		return this.salas[sala].recomendarButacasContiguas(noButacas, sesion);
+		return this.salas[sala - 1].recomendarButacasContiguas(noButacas, sesion);
 	}
 
 	public void comprarEntradasRecomendadas(int sala, int sesion, ButacasContiguas butacas) {
-		this.salas[sala].comprarEntradasRecomendadas(sesion, butacas);
+		this.salas[sala - 1].comprarEntradasRecomendadas(sesion, butacas);
 	}
 
 	public void incluirSesion(int sala, String horaSesion) {
-		this.salas[sala].incluirSesion(horaSesion);
+		this.salas[sala - 1].incluirSesion(horaSesion);
 	}
 
 	public void borrarSesion(int sala, String horaSesion) {
-		this.salas[sala].borrarSesion(horaSesion);
+		this.salas[sala - 1].borrarSesion(horaSesion);
 	}
 }
+
